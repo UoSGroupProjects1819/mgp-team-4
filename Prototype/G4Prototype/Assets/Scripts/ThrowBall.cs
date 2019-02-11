4ce15810ae6f3181212dcec2c3cbd9036a6417c5
alex-turnbull <alexturnbull1@virginmedia.com>
2019-02-11 21:05:07 +0000

    begun work on throwing the ball, just spawns at the moment

---
+++ Prototype/G4Prototype/Assets/Scripts/ThrowBall.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThrowBall : MonoBehaviour {
+
+    public GameObject ballPrefab;
+    [SerializeField]
+    private float offset;
+
+
+	void Start () {
+		
+	}
+	
+	void OnMouseDown () {
+	}
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector3 spawnVector = new Vector3(transform.parent.position.x, transform.parent.position.y, transform.parent.forward.z + offset);
+            Instantiate(ballPrefab, spawnVector, Quaternion.identity);
+        }
+        
+    }
+}
