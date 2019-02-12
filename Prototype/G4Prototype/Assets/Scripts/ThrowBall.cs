717b0eb4ac3607d8bc2bee6d734673ffb43ac5d1
alex-turnbull <alexturnbull1@virginmedia.com>
2019-02-12 16:24:07 +0000

    updated throwing mechanic scripts

--- Prototype/G4Prototype/Assets/Scripts/ThrowBall.cs
+++ Prototype/G4Prototype/Assets/Scripts/ThrowBall.cs
@@ -4,29 +4,54 @@ using UnityEngine;
 
 public class ThrowBall : MonoBehaviour {
 
-    public GameObject ballPrefab;
-    [SerializeField]
-    private float offset;
+    public GameObject ballObject;
     [SerializeField]
     private float force;
+
+    private Vector3 initialPos;
+    private Quaternion initialRot;
 
 
 
     void Start () {
-		
-	}
+        initialPos = ballObject.transform.localPosition;
+        initialRot = ballObject.transform.localRotation;
+    }
 	
 	void OnMouseDown () {
 	}
 
     void Update()
     {
+        if (Input.GetButtonDown("Jump"))
+        {
+            print(initialPos);
+        }
+
         if (Input.GetMouseButtonDown(0))
         {
-            GameObject b = Instantiate(ballPrefab, transform.position + transform.forward * offset, transform.rotation);
-            Rigidbody bRb = b.GetComponent<Rigidbody>();
-            bRb.AddForce(b.transform.position + b.transform.forward * force, ForceMode.Impulse);
+            ballObject.transform.parent = null;
+            Rigidbody ballRB = ballObject.GetComponent<Rigidbody>();
+            ballRB.useGravity = true;
+            ballRB.AddForce(ballObject.transform.position + ballObject.transform.forward * force, ForceMode.Impulse);
         }
+        if (Input.GetMouseButtonDown(1))
+        {
+            resetBall(ballObject);
+        }
+
+        Debug.DrawRay(ballObject.transform.position, ballObject.transform.forward * force, Color.red);
         
+    }
+
+    void resetBall(GameObject ball)
+    {
+        ball.transform.parent = this.gameObject.transform;
+        ball.transform.localPosition = initialPos;
+        ball.transform.localRotation = initialRot;
+        Rigidbody ballRB2 = ball.GetComponent<Rigidbody>();
+        ballRB2.useGravity = false;
+        ballRB2.velocity = Vector3.zero;
+        ballRB2.angularVelocity = Vector3.zero;
     }
 }
